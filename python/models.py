d683e1c7fb42c68ad4d27d7b4a4f195df5a5dfec
Nadav J Outmezguine <Nadav.Out@gmail.com>
2024-05-04 22:57:21 -0700

    working version 1

--- python/models.py
+++ python/models.py
@@ -21,12 +21,12 @@ class FashionCNN(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x,2,stride=2)
+        x = F.max_pool2d(x,2,stride=2,return_indices=True)[0]
         x = self.bn1(x)
 
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x,2,stride=2)
+        x = F.max_pool2d(x,2,stride=2,return_indices=True)[0]
         x = self.bn2(x)
 
         #Flatten
@@ -36,5 +36,26 @@ class FashionCNN(nn.Module):
         x = F.relu(x)
         x = self.fc2(x)
         x = F.relu(x)
+
+        return F.log_softmax(x,dim=-1)
+
+class simpleNN(nn.Module):
+    def __init__(self):
+        super(simpleNN,self).__init__()
+        self.conv   = nn.Conv2d(1,32,3,padding=1,bias=False)
+        self.bn     = nn.BatchNorm2d(32)
+        
+        self.mp     = nn.MaxPool2d(2,stride=2)
+        self.fc     = nn.Linear(32*14*14,10)
+    
+    def forward(self,x):
+        x   = self.conv(x)
+        x   = F.relu(x)
+        x   = self.mp(x)
+        x   = self.bn(x)
+        x   = x.view(x.shape[0],-1)
+        print(x.shape)
+        x   = self.fc(x)
+        x   = F.relu(x)
 
         return F.log_softmax(x,dim=-1)
