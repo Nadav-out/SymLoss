e468ec70f8c38fb5bc7dc16ce2adbe63a89f9239
Inbar Savoray <118119312+inbarsavoray@users.noreply.github.com>
2024-06-04 20:49:56 -0700

    more save and run

---
+++ Inbar/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -C gpu
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=32
+#SBATCH --gpus-per-node=4
+#SBATCH --time 30
+#SBATCH -J train-pm
+#SBATCH -o logs/%x-%j.out
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+
+
+module load parallel
+
+srun parallel --jobs 5 /global/homes/i/inbarsav/SymLoss/Inbar/run_training_for_batch.sh /global/homes/i/inbarsav/SymLoss/Inbar/config_run.json
+
+#srun parallel --jobs 5 /global/homes/i/inbarsav/SymLoss/Inbar/run_training_for_batch.sh /global/homes/i/inbarsav/SymLoss/Inbar/config_run_equiv.json
+
+#srun parallel --jobs 5 /global/homes/i/inbarsav/SymLoss/Inbar/run_training_for_batch.sh /global/homes/i/inbarsav/SymLoss/Inbar/config_run_equiv_skip.json
